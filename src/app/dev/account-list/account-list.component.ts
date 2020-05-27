c2b5065b4487b41b2e48f26e9ebc660511301ee3
a0230288 <uchakravarty@ti.com>
2020-05-27 12:22:19 +0530

    day 9: basic rxjs in dev mode

--- src/app/dev/account-list/account-list.component.ts
+++ src/app/dev/account-list/account-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { AccountsService } from '../services/accounts.service';
 import { LoggingService } from '../services/logging.service';
+import { Subscription, Observable, Subscriber, Subject } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-account-list',
@@ -9,12 +11,108 @@ import { LoggingService } from '../services/logging.service';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	providers: [AccountsService, LoggingService]
 })
-export class AccountListComponent implements OnInit {
+export class AccountListComponent implements OnInit, OnDestroy {
+	private intervalSubscription: Subscription;
+	private subjectSubscription: Subscription;
+	private subjectCount: number;
+
 	accountList: { name: string; status: string }[] = [];
+	customSubject = new Subject<number>();
 
 	constructor(private accountService: AccountsService) {}
 
 	ngOnInit(): void {
 		this.accountList = this.accountService.getAccountList();
+
+		this.createAndSubscribeToCustomObservable();
+
+		this.subscribeToSubject();
+	}
+
+	passNewValueFromSubject() {
+		if (!this.subjectCount) {
+			console.log('subject started');
+			this.subjectCount = 0;
+		}
+		if (this.subjectCount < 0) {
+			this.customSubject.error(new Error('negative numbers not allowed'));
+		} else if (this.subjectCount > 5) {
+			this.customSubject.complete();
+		} else {
+			this.customSubject.next(this.subjectCount++);
+		}
+	}
+
+	subscribeToSubject() {
+		this.subjectSubscription = this.customSubject
+			.pipe(
+				filter((data: number) => {
+					return data % 2 === 0;
+				}),
+				map((data: number) => {
+					return 'Subject passed ' + (data + 1);
+				})
+			)
+			.subscribe(
+				data => {
+					console.log(data);
+				},
+				error => {
+					console.log(error.message);
+				},
+				() => {
+					console.log('subject completed');
+				}
+			);
+	}
+
+	createAndSubscribeToCustomObservable() {
+		console.log('observable started');
+		let count = 0;
+		// creating observable
+		const customObservable = new Observable((observer: Subscriber<any>) => {
+			setInterval(() => {
+				observer.next(count);
+				if (count >= 4) {
+					observer.complete();
+				}
+				if (count < 0) {
+					observer.error(new Error('negative numbers not allowed'));
+				}
+				count++;
+			}, 500);
+		});
+
+		// observable operators & subscribing to it
+		this.intervalSubscription = customObservable
+			.pipe(
+				filter((data: number) => {
+					return data % 2 === 0;
+				}),
+				map((data: number) => {
+					return 'Round ' + (data + 1);
+				})
+			)
+			.subscribe(
+				data => {
+					console.log(data);
+				},
+				error => {
+					console.log(error.message);
+				},
+				() => {
+					console.log('observable completed');
+				}
+			);
+	}
+
+	ngOnDestroy() {
+		this.intervalSubscription.unsubscribe();
+		this.subjectSubscription.unsubscribe();
 	}
 }
+
+/**
+ * update all service event emitters to subjects
+ * add other rxjs operators here
+ */
